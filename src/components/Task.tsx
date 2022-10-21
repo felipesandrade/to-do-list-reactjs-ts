ae88e64ff2b1e8f26cf2df4d9123dbc6c3e4bebf
Felipe Viana de Andrade <felipes.andrade@gmail.com>
2022-10-21 09:28:37 -0300

    finished logic iscompleted

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,18 +1,34 @@
 import { Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { BsFillCheckCircleFill } from 'react-icons/bs';
 
 import styles from './Task.module.css';
 
 export function Task() {
-    const isCompleted = false;
+    const [isCompleted, setIsCompleted] = useState(false);
+
+    function handleTaskCompleted() {
+        if(!isCompleted) {
+            setIsCompleted(true);
+        } else {
+            setIsCompleted(false);
+        }
+
+    }
 
     return (
         <div className={styles.task}> 
-           <button className={styles.checkContainer}>
+           <button 
+                className={styles.checkContainer} 
+                onClick={handleTaskCompleted}
+            >
                 {isCompleted ? <BsFillCheckCircleFill /> : <div /> }
            </button>
             <p className={isCompleted ? styles.checkText : ""}>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-            <Trash />
+            <Trash 
+                className={styles.trash}
+                size={24}
+            />
         </div>
         
     );
